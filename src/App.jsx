2d8f2ec9d899c487be2e7e9d61c9143510bfe665
Omkottewar <omkottewar19.04@gmail.com>
2024-09-19 15:34:41 +0530

    second Commit setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
-import './index.css'
-
+import { useState } from 'react';
+import './index.css';
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import {Actors,Movies,MovieInformation,Profile,Navbar} from './components/index'
 function App() {
 
   return (
-  <div className="">helo</div>
+    <>
+     <BrowserRouter>
+    <main>
+    <Navbar/>
+      <Routes>
+      <Route path='/' element={<Movies/>} />
+      <Route path='/movie/:id' element={<MovieInformation/>} />
+      <Route path='/actors/:id' element={<Actors/>} />
+      <Route path='/profile/:id' element={<Profile/>} />
+      </Routes>
+    </main>
+    </BrowserRouter>
+    </>
   )
+  
 }
 
 export default App
